46efe3c47d93696f9b99dfd2a7d2e889e170615d
Melal1 <105118661+Melal1@users.noreply.github.com>
2023-05-01 11:06:22 +0300

    save

--- telescope/ThemeSwitcher/themepick.lua
+++ telescope/ThemeSwitcher/themepick.lua
@@ -1,5 +1,5 @@
 local has_telescope, telescope = pcall(require, "telescope")
-
+local last_selected = nil
 -- TODO: make dependency errors occur in a better way
 if not has_telescope then
 	error("This plugin requires telescope.nvim (https://github.com/nvim-telescope/telescope.nvim)")
@@ -28,17 +28,32 @@ local lay = {
 	sorting_strategy = "ascending",
 }
 
+local function load_last_selected(prompt_bunfr)
+	local f = io.open("/home/melal/.config/nvim/last_selected.txt", "r")
+	if f then
+		last_selected = f:read("*all")
+		f:close()
+		vim.cmd("colorscheme " .. last_selected)
+		-- vim.notify("The theme " .. last_selected .. " has been selected", "Info", { title = "Theme switcher " })
+	end
+	ac.close(prompt_bunfr)
+end
 -- when press enter
 
 function enter(prompt_bunfr)
 	local sel = ac_st.get_selected_entry() -- get the theme name
+	last_selected = sel[1]
+	local f = io.open("/home/melal/.config/nvim/last_selected.txt", "w")
+	f:write(last_selected)
+	f:close()
 	local cmd = "colorscheme " .. sel[1]
+
 	local save = "sed -i '$d' " .. colorschmefile .. " && echo 'vim.cmd([[" .. cmd .. "]])' >> " .. colorschmefile -- delete the last line on colorscheme file and replace
 
 	vim.cmd(cmd)
 	vim.fn.jobstart(save) -- for no error
 	ac.close(prompt_bunfr)
-	vim.notify("The theme " .. sel[1] .. " has been selected")
+	vim.notify("The theme " .. sel[1] .. " has been selected", "Info", { title = "Theme switcher " })
 end
 
 -- reload theme on select
@@ -64,6 +79,7 @@ local opts = {
 	attach_mappings = function(prompt_bunfr, map)
 		-- insert mode
 		map("i", "<CR>", enter)
+		map("i", "<C-d>", load_last_selected)
 		map("i", "<C-j>", themepicker_move_next)
 		map("i", "<C-k>", themepicker_move_prev)
 		-- normal mode
@@ -72,6 +88,14 @@ local opts = {
 		map("n", "<C-k>", themepicker_move_prev)
 
 		return true
+	end,
+	on_close = function(prompt_bufnr)
+		local sel = ac_st.get_selected_entry()
+		if sel == nil and last_selected ~= nil then
+			-- no selection made, use last selected theme as default
+			vim.cmd("colorscheme " .. last_selected)
+			vim.notify("The theme " .. last_selected .. " has been selected", "Info", { title = "Theme switcher " })
+		end
 	end,
 }
 
